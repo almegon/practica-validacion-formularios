46112676fb18474d31cf7fd3ca2a656836379a77
alberto <almegon69@hotmail.com>
2015-05-26 20:35:00 +0200

    añadida pass compleja

--- app/scripts/validate.js
+++ app/scripts/validate.js
@@ -2,65 +2,8 @@
 $(document).ready(function() {
 
 
-
-    jQuery.validator.addMethod('nifES', function(value, element) {
-
-        value = value.toUpperCase();
-
-        // Basic format test
-        if (!value.match('((^[A-Z]{1}[0-9]{7}[A-Z0-9]{1}$|^[T]{1}[A-Z0-9]{8}$)|^[0-9]{8}[A-Z]{1}$)')) {
-            return false;
-        }
-
-        // Test NIF
-        if (/^[0-9]{8}[A-Z]{1}$/.test(value)) {
-            return ('TRWAGMYFPDXBNJZSQVHLCKE'.charAt(value.substring(8, 0) % 23) === value.charAt(8));
-        }
-        // Test specials NIF (starts with K, L or M)
-        if (/^[KLM]{1}/.test(value)) {
-            return (value[8] === String.fromCharCode(64));
-        }
-
-        return false;
-
-    }, 'Please specify a valid NIF number.');
-
-
-    jQuery.validator.addMethod('nieES', function(value, element) {
-
-        value = value.toUpperCase();
-
-        // Basic format test
-        if (!value.match('((^[A-Z]{1}[0-9]{7}[A-Z0-9]{1}$|^[T]{1}[A-Z0-9]{8}$)|^[0-9]{8}[A-Z]{1}$)')) {
-            return false;
-        }
-
-        // Test NIE
-        //T
-        if (/^[T]{1}/.test(value)) {
-            return (value[8] === /^[T]{1}[A-Z0-9]{8}$/.test(value));
-        }
-
-        //XYZ
-        if (/^[XYZ]{1}/.test(value)) {
-            return (
-                value[8] === 'TRWAGMYFPDXBNJZSQVHLCKE'.charAt(
-                    value.replace('X', '0')
-                    .replace('Y', '1')
-                    .replace('Z', '2')
-                    .substring(0, 8) % 23
-                )
-            );
-        }
-
-        return false;
-
-    }, 'Please specify a valid NIE number.');
-
-
-
-    jQuery.validator.addMethod('cifES', function(value, element) {
-
+    // Metodo para comprobar el cif.
+    jQuery.validator.addMethod('cif', function(value, element) {
         var sum,
             num = [],
             controlDigit;
@@ -95,7 +38,156 @@ $(document).ready(function() {
 
         return false;
 
-    }, 'Please specify a valid CIF number.');
+    }, 'CIF incorrecto');
+
+    // Metodo para comprobar el nif.
+    jQuery.validator.addMethod('nif', function(value, element) {
+        value = value.toUpperCase();
+
+        // Basic format test
+        if (!value.match('((^[A-Z]{1}[0-9]{7}[A-Z0-9]{1}$|^[T]{1}[A-Z0-9]{8}$)|^[0-9]{8}[A-Z]{1}$)')) {
+            return false;
+        }
+
+        // Test NIE
+        //T
+        if (/^[T]{1}/.test(value)) {
+            return (value[8] === /^[T]{1}[A-Z0-9]{8}$/.test(value));
+        }
+
+        //XYZ
+        if (/^[XYZ]{1}/.test(value)) {
+            return (
+                value[8] === 'TRWAGMYFPDXBNJZSQVHLCKE'.charAt(
+                    value.replace('X', '0')
+                    .replace('Y', '1')
+                    .replace('Z', '2')
+                    .substring(0, 8) % 23
+                )
+            );
+        }
+
+        return false;
+    }, 'NIF incorrecto.');
+
+    // Metodo para comprobar el IBAN.
+    jQuery.validator.addMethod('iban', function(value, element) {
+        // some quick simple tests to prevent needless work
+        if (this.optional(element)) {
+            return true;
+        }
+        if (!(/^([a-zA-Z0-9]{4} ){2,8}[a-zA-Z0-9]{1,4}|[a-zA-Z0-9]{12,34}$/.test(value))) {
+            return false;
+        }
+
+        // check the country code and find the country specific format
+        var iban = value.replace(/ /g, '').toUpperCase(); // remove spaces and to upper case
+        var countrycode = iban.substring(0, 2);
+        var bbancountrypatterns = {
+            'AL': '\\d{8}[\\dA-Z]{16}',
+            'AD': '\\d{8}[\\dA-Z]{12}',
+            'AT': '\\d{16}',
+            'AZ': '[\\dA-Z]{4}\\d{20}',
+            'BE': '\\d{12}',
+            'BH': '[A-Z]{4}[\\dA-Z]{14}',
+            'BA': '\\d{16}',
+            'BR': '\\d{23}[A-Z][\\dA-Z]',
+            'BG': '[A-Z]{4}\\d{6}[\\dA-Z]{8}',
+            'CR': '\\d{17}',
+            'HR': '\\d{17}',
+            'CY': '\\d{8}[\\dA-Z]{16}',
+            'CZ': '\\d{20}',
+            'DK': '\\d{14}',
+            'DO': '[A-Z]{4}\\d{20}',
+            'EE': '\\d{16}',
+            'FO': '\\d{14}',
+            'FI': '\\d{14}',
+            'FR': '\\d{10}[\\dA-Z]{11}\\d{2}',
+            'GE': '[\\dA-Z]{2}\\d{16}',
+            'DE': '\\d{18}',
+            'GI': '[A-Z]{4}[\\dA-Z]{15}',
+            'GR': '\\d{7}[\\dA-Z]{16}',
+            'GL': '\\d{14}',
+            'GT': '[\\dA-Z]{4}[\\dA-Z]{20}',
+            'HU': '\\d{24}',
+            'IS': '\\d{22}',
+            'IE': '[\\dA-Z]{4}\\d{14}',
+            'IL': '\\d{19}',
+            'IT': '[A-Z]\\d{10}[\\dA-Z]{12}',
+            'KZ': '\\d{3}[\\dA-Z]{13}',
+            'KW': '[A-Z]{4}[\\dA-Z]{22}',
+            'LV': '[A-Z]{4}[\\dA-Z]{13}',
+            'LB': '\\d{4}[\\dA-Z]{20}',
+            'LI': '\\d{5}[\\dA-Z]{12}',
+            'LT': '\\d{16}',
+            'LU': '\\d{3}[\\dA-Z]{13}',
+            'MK': '\\d{3}[\\dA-Z]{10}\\d{2}',
+            'MT': '[A-Z]{4}\\d{5}[\\dA-Z]{18}',
+            'MR': '\\d{23}',
+            'MU': '[A-Z]{4}\\d{19}[A-Z]{3}',
+            'MC': '\\d{10}[\\dA-Z]{11}\\d{2}',
+            'MD': '[\\dA-Z]{2}\\d{18}',
+            'ME': '\\d{18}',
+            'NL': '[A-Z]{4}\\d{10}',
+            'NO': '\\d{11}',
+            'PK': '[\\dA-Z]{4}\\d{16}',
+            'PS': '[\\dA-Z]{4}\\d{21}',
+            'PL': '\\d{24}',
+            'PT': '\\d{21}',
+            'RO': '[A-Z]{4}[\\dA-Z]{16}',
+            'SM': '[A-Z]\\d{10}[\\dA-Z]{12}',
+            'SA': '\\d{2}[\\dA-Z]{18}',
+            'RS': '\\d{18}',
+            'SK': '\\d{20}',
+            'SI': '\\d{15}',
+            'ES': '\\d{20}',
+            'SE': '\\d{20}',
+            'CH': '\\d{5}[\\dA-Z]{12}',
+            'TN': '\\d{20}',
+            'TR': '\\d{5}[\\dA-Z]{17}',
+            'AE': '\\d{3}\\d{16}',
+            'GB': '[A-Z]{4}\\d{14}',
+            'VG': '[\\dA-Z]{4}\\d{16}'
+        };
+        var bbanpattern = bbancountrypatterns[countrycode];
+        // As new countries will start using IBAN in the
+        // future, we only check if the countrycode is known.
+        // This prevents false negatives, while almost all
+        // false positives introduced by this, will be caught
+        // by the checksum validation below anyway.
+        // Strict checking should return FALSE for unknown
+        // countries.
+        if (typeof bbanpattern !== 'undefined') {
+            var ibanregexp = new RegExp('^[A-Z]{2}\\d{2}' + bbanpattern + '$', '');
+            if (!(ibanregexp.test(iban))) {
+                return false; // invalid country specific format
+            }
+        }
+        // now check the checksum, first convert to digits
+        var ibancheck = iban.substring(4, iban.length) + iban.substring(0, 4);
+        var ibancheckdigits = '';
+        var leadingZeroes = true;
+        var charAt;
+        for (var i = 0; i < ibancheck.length; i++) {
+            charAt = ibancheck.charAt(i);
+            if (charAt !== '0') {
+                leadingZeroes = false;
+            }
+            if (!leadingZeroes) {
+                ibancheckdigits += '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(charAt);
+            }
+        }
+        // calculate the result of: ibancheckdigits % 97
+        var cRest = '';
+        var cOperator = '';
+        for (var p = 0; p < ibancheckdigits.length; p++) {
+            var cChar = ibancheckdigits.charAt(p);
+            cOperator = '' + cRest + '' + cChar;
+            cRest = cOperator % 97;
+        }
+        return cRest === 1;
+    }, 'IBAN incorrecto.');
+
 
 
 
@@ -122,23 +214,41 @@ $(document).ready(function() {
             cifES: 'Por favor, escribe un CIF válido.',
         }),
 
+        // Metodo para comprobar Codigo Postal.
         $('#cp').focusout(function() {
-            var caracteres = $('#cp').val();
-            if (caracteres.length === 4)
-                $('#cp').val('0' + caracteres);
-        }),
+            if ($('#pais option:selected').val() === 'ES/0/0') {
+                if ($(this).val() !== '') {
+                    var dato = $(this).val();
+                    if (dato.length >= 2) {
+                        dato = dato.substring(0, 2);
+                    }
+                    $('#provincia').val(dato);
+                    $('#localidad').val($('#provincia option[value=' + dato + ']').text());
+                }
+            }
+            var zip = $(this).val();
+            var resultado = 5 - zip.length;
+            for (var i = 0; i < resultado; i++) {
+                zip = '0' + zip;
+            }
+            $(this).val(zip);
+        });
 
-        $('#cp').change(function(){
-		      if ($('#pais option:selected').val() === 'ES/0/0'){
-			  if($(this).val()!==''){
-	                	var dato = $(this).val();
-	                	if (dato.length >= 2){
-				    dato = dato.substring(0, 2);    
-				}
-				$('#provincia').val(dato);
-	                }
-		      }		
-	 }),
+    // Metodo para hacer la pass compleja.
+    jQuery.validator.addMethod('complexPass', function(value, element) {
+        var level = $('#complex').attr('value');
+        if (level >= 35) {
+            return true;
+        } else {
+            return false;
+        }
+    }, 'Debes usar una contraseña segura.');
+
+    $('#pass').focusin(function() {
+        $('#pass').complexify({}, function(valid, complexity) {
+            $('#complex').attr('value', complexity);
+        });
+    });
 
 
     $('#formulario').validate({
@@ -152,7 +262,8 @@ $(document).ready(function() {
             telefono: {
                 required: true,
                 digits: true,
-                minlength: 9
+                minlength: 9,
+                maxlength: 9
             },
             email: {
                 email: true,
@@ -164,8 +275,21 @@ $(document).ready(function() {
                 required: true,
                 equalTo: '#email'
             },
-            demandante: {
-                required: true,
+            nif: {
+                required: function() {
+                    return $('#particular').is(':checked');
+                },
+                nif: 'nif',
+                remote: 'http://www.futbolistas.com/validar_nif_db'
+            },
+            cif: {
+                required: function() {
+                    return $('#empresa').is(':checked');
+                },
+                cif: 'cif'
+            },
+            nombreempresa: {
+                required: true
             },
             direccion: {
                 required: true,
@@ -179,30 +303,26 @@ $(document).ready(function() {
             pais: {
                 required: true,
             },
-            cif: {
-                nifES: true,
-                required: true
-            },
             iban: {
-                iban: true,
-                minlength : 20,
-				maxlength : 20,
-				required: function() {
-						return $('#tipopagocuenta').is(':checked');
-					},
-				validacuentabanco: function() {
-					return $('#tipopagocuenta').is(':checked');
-				}
-			},
+                required: true,
+                iban: 'iban'
             },
+
             usuario: {
                 usuario: true,
                 required: true,
-                remote: 'http://www.futbolistas.com/validar_nif_db.php'
+                remote: 'http://www.futbolistas.com/validar_nif_db.php',
+                minlength: 4
             },
             password: {
                 password: true,
-                required: true
+                required: true,
+                complexPass: true,
+                minlength: 6
+            },
+            password2: {
+                required: true,
+                equalTo: '#password'
             },
             cp: {
                 cp: true,
@@ -214,11 +334,12 @@ $(document).ready(function() {
             }
 
         },
+        submitHandler: function() {
+            alert('¡Envíado!');
+        }
+
 
 
     });
-    submitHandler: function() {
-        alert('¡Envíado!');
-    }
 
 });
